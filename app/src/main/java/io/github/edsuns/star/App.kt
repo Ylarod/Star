6ffd301f75e94ae70ac079a3b20917d7eabdf9ac
Ylarod <me@ylarod.cn>
2022-04-19 21:44:11 +0800

    add Bugly for crash report

--- app/src/main/java/io/github/edsuns/star/App.kt
+++ app/src/main/java/io/github/edsuns/star/App.kt
@@ -1,6 +1,9 @@
 package io.github.edsuns.star
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import com.tencent.bugly.crashreport.CrashReport
 
 /**
  * Created by Edsuns@qq.com on 2021/6/27.
@@ -10,6 +13,12 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        CrashReport.initCrashReport(this, "ce159ef1e2", false)
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 
     companion object {
